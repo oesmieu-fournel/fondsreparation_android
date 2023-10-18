9f2946f57df9a802f7f63d232a03e6ff0f29b7d0
YohannWEBER <yohann.weber@gmail.com>
2023-10-18 15:49:05 +0200

    Refactoring AWSCertifiedFilesApi to be able to upload the file on AWS

--- app/src/main/java/com/ecosystem/mobile/reparation/service/api/aws_file_upload/models/AWSCertifiedUploadLink.kt
+++ app/src/main/java/com/ecosystem/mobile/reparation/service/api/aws_file_upload/models/AWSCertifiedUploadLink.kt
@@ -1,6 +1,6 @@
 package com.ecosystem.mobile.reparation.service.api.aws_file_upload.models
 
-class AWSUploadLink(
+class AWSCertifiedUploadLink(
     val url : String,
     val fileLocation : String,
 )

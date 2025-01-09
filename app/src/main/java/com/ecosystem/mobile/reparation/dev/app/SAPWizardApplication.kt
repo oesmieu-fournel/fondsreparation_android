016c3a9828c0c8d89c54ae81afb3442bda3b6bbd
Yohann Weber <yohann.weber@gmail.com>
2025-01-09 17:56:59 +0100

    FirebaseService initialization with background and foreground interceptor

--- app/src/main/java/com/ecosystem/mobile/reparation/dev/app/SAPWizardApplication.kt
+++ app/src/main/java/com/ecosystem/mobile/reparation/dev/app/SAPWizardApplication.kt
@@ -1,36 +1,47 @@
 package com.ecosystem.mobile.reparation.dev.app
 
+import android.app.Activity
 import android.app.Application
+import android.content.Context
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Lifecycle
 import com.ecosystem.mobile.reparation.dev.BuildConfig
 import com.ecosystem.mobile.reparation.dev.data.SharedPreferenceRepository
 import com.ecosystem.mobile.reparation.dev.repository.RepositoryFactory
+import com.ecosystem.mobile.reparation.dev.ui.CustomFlutterActivity
+import com.ecosystem.mobile.reparation.dev.ui.push_notifications.PushNotificationAlertDialog
+import com.sap.cloud.mobile.foundation.authentication.AppLifecycleCallbackHandler
 import com.sap.cloud.mobile.foundation.logging.LoggingService
 import com.sap.cloud.mobile.foundation.mobileservices.MobileService
 import com.sap.cloud.mobile.foundation.mobileservices.SDKInitializer
 import com.sap.cloud.mobile.foundation.mobileservices.ServiceListener
 import com.sap.cloud.mobile.foundation.mobileservices.ServiceResult
 import com.sap.cloud.mobile.foundation.model.AppConfig
+import com.sap.cloud.mobile.foundation.remotenotification.BasePushService
+import com.sap.cloud.mobile.foundation.remotenotification.FirebasePushService
+import com.sap.cloud.mobile.foundation.remotenotification.PushCallbackListener
+import com.sap.cloud.mobile.foundation.remotenotification.PushRemoteMessage
 import com.sap.cloud.mobile.foundation.settings.policies.LogPolicy
 import com.sap.cloud.mobile.foundation.theme.ThemeDownloadService
-import com.sap.cloud.mobile.foundation.user.DeviceUser
-import com.sap.cloud.mobile.foundation.user.User
-import com.sap.cloud.mobile.foundation.user.UserService
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.dart.DartExecutor
-import io.flutter.embedding.engine.loader.FlutterLoader
 import io.flutter.view.FlutterMain
 import okhttp3.OkHttpClient
+import org.slf4j.LoggerFactory
 
 
-class SAPWizardApplication: Application() {
+class SAPWizardApplication : Application() {
 
     internal var isApplicationUnlocked = false
 
-    lateinit var httpClient : OkHttpClient
+    lateinit var httpClient: OkHttpClient
     lateinit var appConfig: AppConfig
-    lateinit var flutterEngine : FlutterEngine
+    lateinit var flutterEngine: FlutterEngine
+    var pushRemoteMessage: PushRemoteMessage? = null
+    lateinit var firebasePushService: FirebasePushService
 
 
     override fun onCreate() {
@@ -76,10 +87,58 @@ class SAPWizardApplication: Application() {
             logToConsole = true
         })
 
+        firebasePushService = FirebasePushService().apply {
+            isEnableAutoMessageHandling = true
+            setPushCallbackListener(object : PushCallbackListener {
+                override fun onReceive(context: Context, message: PushRemoteMessage) {
+                    pushRemoteMessage = message
+                }
+
+
+            })
+            backgroundNotificationInterceptor = object :
+                BasePushService.BackgroundNotificationInterceptor {
+                override fun onBackgroundNotificationMessageReceived(pushNotificationEvent: BasePushService.PushNotificationEvent) {
+                    pushNotificationEvent.displayNotification(pushNotificationEvent.pushRemoteMessage)
+                    pushRemoteMessage = pushNotificationEvent.pushRemoteMessage
+                }
+            }
+            foregroundNotificationInterceptor =
+                object : BasePushService.ForegroundNotificationInterceptor {
+                    override fun onForegroundNotificationMessageReceived(pushNotificationEvent: BasePushService.PushNotificationEvent) {
+                        if(pushNotificationEvent.pushRemoteMessage == null){
+                            Log.d("SAPWizardApplication", "pushRemoteMessage is null")
+                            return
+                        }
+                        val message = pushNotificationEvent.pushRemoteMessage!!
+                        val foregroundActivity = AppLifecycleCallbackHandler.getInstance().activity as FlutterActivity?
+                        //if we can display the push message in the foreground
+                        if( foregroundActivity != null && foregroundActivity.lifecycle?.currentState?.isAtLeast(Lifecycle.State.RESUMED) == true) {
+                            foregroundActivity.runOnUiThread {
+                                PushNotificationAlertDialog()(
+                                    foregroundActivity,
+                                    message,
+                                    this@apply
+                                ).show()
+                            }
+                            pushRemoteMessage = null
+                        }
+                        else{
+                            //if we can't display the push message, we stock it to display it later
+                            pushRemoteMessage = message
+                        }
+
+
+                    }
+
+                }
+
+        }
+        services.add(firebasePushService)
+
         SDKInitializer.start(this, * services.toTypedArray())
     }
-
-    private fun initFlutterEngine(){
+    private fun initFlutterEngine() {
         // Instantiate a FlutterEngine.
         flutterEngine = FlutterEngine(this)
 
@@ -88,7 +147,8 @@ class SAPWizardApplication: Application() {
             //DartExecutor.DartEntrypoint.createDefault()
             DartExecutor.DartEntrypoint(
                 FlutterMain.findAppBundlePath(),
-                BuildConfig.FLAVOR)
+                BuildConfig.FLAVOR
+            )
         )
 
         // Cache the FlutterEngine to be used by FlutterActivity.
@@ -100,5 +160,6 @@ class SAPWizardApplication: Application() {
 
     companion object {
         const val KEY_LOG_SETTING_PREFERENCE = "key.log.settings.preference"
+        internal val logger = LoggerFactory.getLogger(SAPWizardApplication::class.java)
     }
 }
